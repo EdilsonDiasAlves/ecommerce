5486a26628186d13ea4b04d61cb53d4dea6f64c2
Edilson Moizinho <edilsondiasalves@outlook.com>
2022-05-21 00:44:31 -0300

    Paralelizando consumo com particoes e chaves

--- src/main/java/br/com/diy/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/diy/ecommerce/NewOrderMain.java
@@ -7,30 +7,38 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
-        // idPedido, idUsuario, valor
-        var value = "6, 5, 17.00";
-        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        Callback callback = (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
-            }
-            System.out.println("sucesso no envio: "
-                + "| topic: " + data.topic()
-                + "| partition: " + data.partition()
-                + "| offset: " + data.offset()
-                + "| " + data.timestamp());
-        };
-        var email = "Obrigado! Estamos processando sua ordem!";
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
-        producer.send(record, callback).get();
-        producer.send(emailRecord, callback).get();
+
+        var qtdMsgs = 10;
+        for (var i = 0; i < qtdMsgs; i++) {
+
+
+            // idPedido, idUsuario, valor
+            var key = UUID.randomUUID().toString();
+            var value = key + ", 2, 10.00";
+            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
+            Callback callback = (data, ex) -> {
+                if (ex != null) {
+                    ex.printStackTrace();
+                    return;
+                }
+                System.out.println("sucesso no envio: "
+                    + "| topic: " + data.topic()
+                    + "| partition: " + data.partition()
+                    + "| offset: " + data.offset()
+                    + "| " + data.timestamp());
+            };
+            var email = "Obrigado! Estamos processando sua ordem!";
+            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
+            producer.send(record, callback).get();
+            producer.send(emailRecord, callback).get();
+        }
     }
 
     private static Properties properties() {
